58f099a1bfc42b094d1ec2565e8535cfd869862c
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-04-08 22:49:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function firstWord(s) {
   // your code here
-	s.trim(); 
+		s=s.trim(); 
 	let a=s.split(' ');
-	return a[0]
-}
+	return a[0] 
 
+}
+ 
 // Do not change the code below
 
 const s = prompt("Enter String:");
